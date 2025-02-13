65434b127dea2f9fa2edad6c227cb9c45b0a2ab4
PyskoMaxym <pisko.maks@gmail.com>
2025-02-13 20:47:58 +0200

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
+import { useEffect,lazy } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { refreshUser } from "./redux/auth/operations";
 import { selectIsRefreshing } from "./redux/auth/selectors";
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Layout from "./components/Layout/Layout";
-import HomePage from "./pages/HomePage/HomePage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import ContactsPage from "./pages/ContactsPage/ContactsPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"; 
+// import HomePage from "./pages/HomePage/HomePage";
+// import RegisterPage from "./pages/RegisterPage/RegisterPage";
+// import LoginPage from "./pages/LoginPage/LoginPage";
+// import ContactsPage from "./pages/ContactsPage/ContactsPage";
+// import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"; 
 import PrivateRoute from "./routes/PrivateRoute";
 import RestrictedRoute from "./routes/RestrictedRoute";
 import CircularProgress from "@mui/material/CircularProgress";
+
+const HomePage = lazy(()=> import('./pages/HomePage/HomePage'));
+const RegisterPage = lazy(()=> import('./pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy (()=> import('./pages/LoginPage/LoginPage'));
+const NotFoundPage = lazy (()=> import ('./pages/NotFoundPage/NotFoundPage'));
+const ContactsPage = lazy (()=> import('./pages/ContactsPage/ContactsPage'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -33,7 +39,7 @@ const App = () => {
       <CircularProgress />
     </div>
   ) : (
-    <>
+    <Layout>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
@@ -65,7 +71,7 @@ const App = () => {
         </Route>
       </Routes>
       <Toaster position="top-right" reverseOrder={false} />
-    </>
+    </Layout>
   );
 };
 
